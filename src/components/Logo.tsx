b85d32f59ffb3982e882bf5599cf6721b89d1d41
Andrey <shock.work@mail.ru>
2021-04-04 10:00:14 +0300

    template for logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logoImg from '../assets/img/logo.png';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src={logoImg} alt="Logo" />
+        </div>
+    );
+};
+
+export default Logo;
