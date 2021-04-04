942828426d5e80f34304963c18111f3ab450cc5c
Andrey <shock.work@mail.ru>
2021-04-04 18:04:28 +0300

    ban a click when a download is in progress

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
+import { useSelector } from 'react-redux';
+import { ApplicationState } from '../store';
 
 const tabs = [
     'Самый дешевый',
@@ -8,10 +10,11 @@ const tabs = [
 ];
 
 const Tabs = () => {
+    const { loading }: any = useSelector<ApplicationState>(state => state.tickets);
     const [activeTab, setActiveTab] = useState<number>(0);
 
     const clickTabHandler = (index: number): void => {
-        if (activeTab !== index) {
+        if (activeTab !== index && !loading) {
             setActiveTab(index);
         }
     };
@@ -21,7 +24,7 @@ const Tabs = () => {
             {tabs.map((name, index) =>
                 <div
                     key={index}
-                    className={classNames('tabs__item', { active: activeTab === index })}
+                    className={classNames('tabs__item', { active: activeTab === index, disabled: loading })}
                     onClick={clickTabHandler.bind(null, index)}
                 >
                     {name}
