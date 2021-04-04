380511eefb70b1745d2e38fee17895192e869ff9
Andrey <shock.work@mail.ru>
2021-04-04 12:03:16 +0300

    template for Tabs component

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+const tabs = [
+    'Самый дешевый',
+    'Самый быстрый',
+    'Оптимальный'
+]
 
 const Tabs = () => {
+    const [activeTab, setActiveTab] = useState<number>(0);
+
+    const clickTabHandler = (index: number): void => {
+        if (activeTab !== index) {
+            setActiveTab(index);
+        }
+    };
+
     return (
         <div className="tabs">
-
+            {tabs.map((name, index) =>
+                <div
+                    className={classNames('tabs__item', { active: activeTab === index })}
+                    onClick={clickTabHandler.bind(null, index)}
+                >
+                    {name}
+                </div>
+            )}
         </div>
     );
 };
