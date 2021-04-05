2dea6127b0f79dedeef1c90c433fe56d45e40cc2
Andrey <shock.work@mail.ru>
2021-04-05 15:45:51 +0300

    update global store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import tickets from './tickets/reducer';
 import { TicketsState } from './tickets/types';
+import { FiltersState } from './filters/types';
+import tickets from './tickets/reducer';
+import filters from './filters/reducer';
 
 export interface ApplicationState {
-    tickets: TicketsState
+    tickets: TicketsState,
+    filters: FiltersState
 }
 
 const root = combineReducers({
-    tickets
+    tickets,
+    filters
 });
 
 const store = createStore(
