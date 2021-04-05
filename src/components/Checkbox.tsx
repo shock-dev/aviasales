96ea10e8f0af145b75ab34db28d5eb8e04ccfe25
Andrey <shock.work@mail.ru>
2021-04-05 18:57:21 +0300

    change template of Checkbox component and more

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import classNames from 'classnames';
 
 interface CheckboxInterface {
     readonly name: string
+    readonly val: string
+    readonly checked: boolean
     readonly loading: boolean
+    onClickCheckbox: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const Checkbox = ({ name, loading }: CheckboxInterface) => {
+const Checkbox = ({ name, checked, loading, onClickCheckbox }: CheckboxInterface) => {
     return (
-        <label className="filters__item-label checkbox">
-            <input
-                className="checkbox__real"
-                type="checkbox"
-                disabled={loading}
-            />
-            <span className="checkbox__fake" />
-            <span className="checkbox__text">
-                {name}
-            </span>
-        </label>
+        <li className={classNames('filters__item', { disabled: loading })}>
+            <label className="filters__item-label checkbox">
+                <input
+                    className="checkbox__real"
+                    type="checkbox"
+                    onChange={(e) => onClickCheckbox(e)}
+                    disabled={loading}
+                    checked={checked}
+                />
+                <span className="checkbox__fake" />
+                <span className="checkbox__text">
+                    {name}
+                </span>
+            </label>
+        </li>
     );
 };
 
