1fa91e5585d60b6180272ba6afbb88990eb4b52e
Andrey <shock.work@mail.ru>
2021-04-06 12:34:45 +0300

    actions for filters

--- src/store/filters/actions.ts
+++ src/store/filters/actions.ts
@@ -4,3 +4,13 @@ export const setSortBy = (val: string) => ({
     type: FiltersActionTypes.SET_SORT_BY,
     payload: val
 });
+
+export const setFilter = (val: string) => ({
+    type: FiltersActionTypes.SET_FILTER,
+    payload: val
+});
+
+export const clearFilter = (val: string) => ({
+    type: FiltersActionTypes.CLEAR_FILTER,
+    payload: val
+});
