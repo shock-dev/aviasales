4d56be61ff2c20d80fc04123eea9d1042dd31f32
Andrey <shock.work@mail.ru>
2021-04-20 16:33:04 +0300

    reducer of tickets realized

--- src/store/tickets/reducer.ts
+++ src/store/tickets/reducer.ts
@@ -1,11 +1,28 @@
 import produce from 'immer';
+import { TicketsState } from './types/state';
+import { LoadingStatus } from '../types';
+import { TicketsActionType } from './types/actions';
 
-const initialTicketsState: any = {
-  data: undefined
+const initialTicketsState: TicketsState = {
+  items: undefined,
+  loadingStatus: LoadingStatus.NEVER
 };
 
 const tickets = produce((draft, action: any) => {
   switch (action.type) {
+    case TicketsActionType.FETCH_ITEMS:
+      draft.loadingStatus = LoadingStatus.LOADING;
+      break;
+
+    case TicketsActionType.SET_ITEMS:
+      draft.items = action.payload;
+      draft.loadingStatus = LoadingStatus.SUCCESS;
+      break;
+
+    case TicketsActionType.SET_LOADING_STATUS:
+      draft.loadingStatus = action.payload;
+      break;
+
     default:
       break;
   }
