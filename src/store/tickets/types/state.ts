ba012dbce32aaaf551df0ca9694795cac9ace895
Andrey <shock.work@mail.ru>
2021-04-20 16:32:49 +0300

    basic interfaces for tickets

---
+++ src/store/tickets/types/state.ts
@@ -0,0 +1,27 @@
+import { LoadingStatus } from '../../types';
+
+export interface ITicket {
+  price: number
+  carrier: string
+  segments: [
+    {
+      origin: string
+      destination: string
+      date: string
+      stops: string[]
+      duration: number
+    },
+    {
+      origin: string
+      destination: string
+      date: string
+      stops: string[]
+      duration: number
+    }
+  ]
+}
+
+export interface TicketsState {
+  items?: ITicket[]
+  loadingStatus: LoadingStatus
+}
