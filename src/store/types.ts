f5b92ee6f9a66d8edd9d8a7ddcb63ec37522d0d0
Andrey <shock.work@mail.ru>
2021-04-20 19:29:48 +0300

    typed RootState

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,6 +1,12 @@
+import { TicketsState } from './tickets/types/state';
+
 export enum LoadingStatus {
   NEVER = 'NEVER',
   SUCCESS = 'SUCCESS',
   ERROR = 'ERROR',
   LOADING = 'LOADING'
+}
+
+export interface RootState {
+  tickets: TicketsState
 }
