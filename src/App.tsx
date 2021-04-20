34dd39db4c1c53a7f7b545d2de98e07b0c1bbc11
Andrey <shock.work@mail.ru>
2021-04-20 16:43:58 +0300

    fetching tickets in saga and dispatch in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Logo from './components/Logo';
 import Filters from './components/Filters';
 import Tabs from './components/Tabs';
 import TicketList from './components/TicketList';
 import { tabs } from './resources/tabs';
 import { filters } from './resources/filters';
+import { useDispatch } from 'react-redux';
+import { fetchTickets } from './store/tickets/actions';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTickets());
+  }, []);
+
   return (
     <div className="app">
       <Logo />
--- src/store/tickets/saga.ts
+++ src/store/tickets/saga.ts
@@ -1,15 +1,21 @@
-import { takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { SagaIterator } from 'redux-saga';
+import { TicketsActionType } from './types/actions';
+import TicketsApi from '../../services/api/tickets.api';
+import { setTickets, setTicketsLoadingState } from './actions';
+import { LoadingStatus } from '../types';
 
-function* fetchTickets() {
+function* fetchTickets(): SagaIterator {
   try {
-    console.log('test');
+    const tickets = yield call(TicketsApi.fetchTickets);
+    yield put(setTickets(tickets));
   } catch (e) {
-    console.log(e);
+    yield put(setTicketsLoadingState(LoadingStatus.ERROR));
   }
 }
 
 function* ticketsSaga() {
-  yield takeEvery('FETCH_TICKETS', fetchTickets);
+  yield takeEvery(TicketsActionType.FETCH_ITEMS, fetchTickets);
 }
 
 export default ticketsSaga;
