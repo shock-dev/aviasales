e1712233abb2f38d8e494a9b81605211b437041b
Andrey <shock.work@mail.ru>
2021-04-21 12:07:08 +0300

    call store when changing checkboxes

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import classNames from 'classnames';
 
 interface CheckboxInterface {
   readonly name: string
-  readonly val: string
+  readonly val: number | string
   readonly checked: boolean
   readonly loading: boolean
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const Checkbox = ({ name, checked, loading }: CheckboxInterface) => {
+const Checkbox = ({ name, checked, loading, onChange }: CheckboxInterface) => {
   return (
     <li className={classNames('filters__item', { disabled: loading })}>
       <label className="filters__item-label checkbox">
         <input
           className="checkbox__real"
           type="checkbox"
+          onChange={(e) => onChange(e)}
           disabled={loading}
           checked={checked}
         />
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,16 +1,44 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent } from 'react';
 import Checkbox from './Checkbox';
-import { FilterDataInterface } from '../resources/filters';
-import { useSelector } from 'react-redux';
+import { filters } from '../resources/filters';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectIsLoadingTickets } from '../store/tickets/selectors';
+import { selectFilterList } from '../store/filters/selectors';
+import { clearFilter, setFilter } from '../store/filters/actions';
 
-interface FiltersProps {
-  readonly data: FilterDataInterface[]
-}
-
-const Filters = ({ data }: FiltersProps) => {
+const Filters = () => {
+  const dispatch = useDispatch();
+  const filterList = useSelector(selectFilterList);
   const isLoading = useSelector(selectIsLoadingTickets);
-  const [filters] = useState<FilterDataInterface[]>(data);
+
+  const changeCheckboxHandler = (e: ChangeEvent<HTMLInputElement>, val: number | string) => {
+    const isChecked = e.target.checked;
+
+    if (val === 'all' && isChecked) {
+      return dispatch(clearFilter());
+    }
+
+    if (val === 'all' && !isChecked) {
+      return dispatch(setFilter([0]));
+    }
+
+    const clone = [...filterList];
+    const index = clone.indexOf('all');
+
+    index !== -1 && clone.splice(index, 1);
+
+    if (e.target.checked) {
+      clone.push(val);
+    } else {
+      clone.splice(clone.indexOf(val), 1);
+
+      if (!clone.length) {
+        return dispatch(clearFilter());
+      }
+    }
+
+    dispatch(setFilter(clone));
+  };
 
   return (
     <div className="filters">
@@ -21,8 +49,11 @@ const Filters = ({ data }: FiltersProps) => {
         {filters.map((item) =>
           <Checkbox
             key={item.id}
+            name={item.name}
+            val={item.val}
+            checked={filterList.includes(item.val)}
             loading={isLoading}
-            {...item}
+            onChange={(e) => changeCheckboxHandler(e, item.val)}
           />
         )}
       </ul>
