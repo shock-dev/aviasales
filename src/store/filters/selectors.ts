8d3f5b6271254c61f7531ad3d07988c36c0358b6
Andrey <shock.work@mail.ru>
2021-04-21 08:37:47 +0300

    selector for getting filter list

--- src/store/filters/selectors.ts
+++ src/store/filters/selectors.ts
@@ -3,3 +3,5 @@ import { RootState } from '../types';
 export const selectFiltersState = (state: RootState) => state.filters;
 
 export const selectSortBy = (state: RootState) => selectFiltersState(state).sortBy;
+
+export const selectFilterList = (state: RootState) => selectFiltersState(state).filters;
