9ea70f0c6ad03a4238d98cc1e2c064b14287f1bd
Andrey <shock.work@mail.ru>
2021-06-08 22:28:56 +0300

    change filename of root reducer and saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,16 @@
 import { applyMiddleware, createStore } from 'redux';
-import root from './reducer.root';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootSaga from './saga.root';
+import root from './reducer';
+import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   root,
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  )
 );
 
 sagaMiddleware.run(rootSaga);
